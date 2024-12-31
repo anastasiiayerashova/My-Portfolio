741f34ba2caac1ac7367835b502b1833f3bf71a2
Anastasiia Yerashova <yerashova.a@gmail.com>
2024-12-31 01:18:13 +0100

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
 import './App.css'
 
 export default function App() {
-  
+    return (
+        <body>
+            
+      </body>
+  )
 }
 
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,3 @@
+import s from './Header.module.css'
+
+export default function Header {}
